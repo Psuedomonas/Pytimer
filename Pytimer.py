0492c1df1e0c35f647c7239d200cc0895ee85ba2
Pseudomonas <zehm0007@d.umn.edu>
2021-03-30 17:38:07 -0500

    Update Pytimer.py

--- Pytimer.py
+++ Pytimer.py
@@ -1,19 +1,74 @@
+#!/usr/bin/python3.8
+# Filename: Pytimer.py
 '''
 Nicholas Zehm
 Pytimer.py
 2021-2-27
 
 Very simple countdown timer script. Requires the vlc python module. Manually set the default sound for your system.
+
 Version 1.1 (2021-3-12)
-Add hours, deal with negative values (the script worked with negative values, due to the t > 0 check) 
+Add hours, deal with negative values (the script worked with negative values, due to the t > 0 check)
+
+Version 1.1.2 (2021-3-27)
+Catch bad playfile, put playfile in global scope. Script not complex enough to for this to be an issue.
+
+Version 1.1.5 (2021-3-30)
+Add main menu and stopwatch method. Alarm method not properly implemented yet.
+Add countdownSetup() to allow repeating countdown without needing additional user input
+Fixed bug with Ctrl-C in countdown with undefined p object
+Fixed typos
+
+Bug: sometimes there is a trailing 0 in the output, caused by decimal place drop in h, mins, or secs - severity: trivial
 '''
 
+#### EDIT THIS: provide a valid file (vlc usable) for playfile. This will be used as default/fallback ####
+playfile = ""
+
+
+#import modules
 import time
 import os   #for path to music/sound
 import vlc  #this module is from the vlc team, and is required for music/sound
 
 
-def countdown():
+#
+#  name: stopwatch
+#  @param none
+#  @return main()
+#
+def stopwatch():
+    t = 0
+    h = 0
+    mins = 0
+    secs = 0
+    print('Type Ctrl-C to stop timer')
+    print("{0}:{1}:{2}".format(h, mins, secs), end ='\r')
+    #Ctrl-C causes KeyboardInterrupt to be raised
+    try:
+        while True:
+            t += 1
+            time.sleep(1)
+            h, m = divmod(t, 3600)
+            mins, secs = divmod(m, 60)
+            print("{0}:{1}:{2}".format(h, mins, secs), end ='\r')
+
+
+    except KeyboardInterrupt:
+        pass
+
+    print('\nStop Time')
+    print("{0}:{1}:{2}".format(h, mins, secs), end ='\r')
+    input("\nPress 'Enter' to return to main menu...")
+    return main()
+
+
+#
+#  name: countdownSetup
+#  @param playfile
+#  @return countdown(playfile, T)
+#
+def countdownSetup(playfile):
     try:
         h = int(input("hours: "))
     except:
@@ -22,7 +77,7 @@ def countdown():
     if h < 0:
         h = 0
         print("0 hours")
-        
+
     try:
         m = int(input("minutes: "))
     except:
@@ -31,7 +86,7 @@ def countdown():
     if m < 0:
         m = 0
         print("0 minutes")
-        
+
     try:
         s = int(input("seconds: "))
     except:
@@ -40,12 +95,9 @@ def countdown():
     if s < 0:
         s = 0
         print("0 seconds")
-        
+
     a = input("User defined alarm sound? y/n: ")
-    
-    #### EDIT THIS: provide a valid file (vlc usable) for playfile. This will be used as default/fallback ####
-    playfile = ""
-    
+
     if a == 'y':
         path = input("Input sound file directory (no ""s): ")
 
@@ -55,21 +107,88 @@ def countdown():
             print("Using {0}".format(playfile))
         else:
             print("Error: Using default sound.")
-            
-    t = h * 60 * 60 + m * 60 + s
 
+    T = h * 60 * 60 + m * 60 + s
+
+    return countdown(playfile, T)
+
+
+#
+#  name: countdown
+#  @param playfile, T
+#  @return main() or countdown
+#
+def countdown(playfile, T):
+    t = T
     #Ctrl-C causes KeyboardInterrupt to be raised
     try:
         while True and t > 0:
             h, m = divmod(t, 3600)
-            mins, secs = divmod(m, 60) 
+            mins, secs = divmod(m, 60)
             print("{0}:{1}:{2}".format(h, mins, secs), end ='\r')
             time.sleep(1)
             t -= 1
         p = vlc.MediaPlayer(playfile)
         p.play()
-        input("Sounding the alarm...")        
+        input("Sounding the alarm...")
     except KeyboardInterrupt:
+        return main()
+
+    p.stop()
+
+    r = input("Repeat countdown? (y/n): ")
+    if r == 'y':
+        return countdown(playfile, T)
+    else:
+        return main()
+
+
+#
+#  name: alarm
+#  @param sound object
+#  @return main()
+#
+def alarm(playfile):
+    print('Alarm not implemented yet')
+    '''
+    r = input("repeat alarm? (y/n): ")
+    if r == 'y':
         pass
-        
-countdown()   
+    else:
+        pass
+    d = input("day(s) (format MTWThFStSn): ")
+    #parse d for chars
+    #set day(s) for alarm
+
+    day = 'M'
+    alarm_day = 'M'
+    t = input("time (format use military time, h:m): ")
+
+    if day == alarm_day:
+        #set an alarm for day at specified time
+    '''
+
+    return main()
+
+
+#
+#  name: main
+#  @param none
+#  @return  function
+#
+def main():
+    choice = input("Press 'a' for alarm, 'c' for countdown timer, 's' for stopwatch, or 'x' to exit: ")
+
+    if choice == 'a':
+        return alarm(playfile)
+    elif choice == 'c':
+        return countdownSetup(playfile)
+    elif choice == 's':
+        return stopwatch()
+    elif choice == 'x':
+        exit()
+    else:
+        main()
+
+
+main()
