cec570c1fbb7eedabcbca33d63fc2d794c66e79b
Pseudomonas <zehm0007@d.umn.edu>
2022-09-23 17:06:27 -0500

    version 1.1.6 clocks class

---
+++ clocks.py
@@ -0,0 +1,25 @@
+'''
+Nicholas Zehm
+2022-4-7
+Clock class
+filename: pyclock.py
+for Pytimer.py
+'''
+
+clocks = {}
+
+class Alarm(self):
+	pass
+	
+class Countdown(self, T):
+	self.T = T
+	
+	def getT(self):
+		return self.T
+		
+	def setT(self):
+		if T < 0:
+			T = 0
+		self.T = T
+		
+	
